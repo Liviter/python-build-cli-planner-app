93ed834bbcec95c7971af642c66de3ea2463dffa
Liviter <77466244+Liviter@users.noreply.github.com>
2023-01-06 16:37:58 +0000

    commit 3
    Double checking as now less tests are passing...

--- src/database.py
+++ src/database.py
@@ -1,6 +1,6 @@
 import csv
-from src.reminder import PoliteReminder
-from src.deadlined_reminders import DateReminder
+from src.deadlined_reminders import DeadlinedReminder
+
 
 def list_reminders():
     f = open("reminders.csv", "r")
@@ -14,8 +14,12 @@ def list_reminders():
                 print(e.ljust(32), end=' ')
         print()
 
-def add_reminder(text, date):
-    reminder = DateReminder(text, date)
+
+def add_reminder(text, date, ReminderClass):
+    reminder = ReminderClass(text, date)
+    if issubclass(isinstance((reminder, DeadlinedReminder))):
+        raise TypeError("Invalid Reminder Class")
+
     with open('reminders.csv', 'a+', newline='\n') as file:
         writer = csv.writer(file)
         writer.writerow(reminder)
